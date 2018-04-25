3d00047939884cede1d9441f8f79274d31397097
elfraim <elfraiman@gmail.com>
2018-04-25 11:41:42 +0200

    Added Player

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDOReG4eGGL2_DCFrbTqbwDfT8M-zLt3TU';
@@ -14,17 +15,21 @@ class App extends Component {
 
     this.state = {
       videos: [],
+      selectedVideo: null,
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+    YTSearch({key: API_KEY, term: 'top fails'}, (data) => {
       this.setState({ videos: data });
     });
   }
+
+
 
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     )
