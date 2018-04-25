e496a2e7acdecb8454ce3afb78ac7a0e9806b0a4
elfraim <elfraiman@gmail.com>
2018-04-25 11:24:54 +0200

    Added Videos + Titles

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 
 
-const VideoListItem = (props) => {
-    return <li>Video</li>
+const VideoListItem = ({video}) => {
+    //const video = props.video; but we used ({video}) GET USED TO THIS!
+    console.log(video)
+    const imageUrl = video.snippet.thumbnails.default.url;
+    const videoHeading = video.snippet.title
+
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl} />
+                </div>
+
+                <div className="media-body">
+                    <div className="media-heading">{videoHeading}</div>
+                </div>
+            </div>
+        </li>
+    )
 };
 
 
